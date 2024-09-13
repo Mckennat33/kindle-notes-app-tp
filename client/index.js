441721b07f8305b09b9e08c8542308c866a4063a
tom mckenna <thomasmckenna12@gmail.com>
2024-09-12 19:41:42 -0500

    added html and client side javascript starter code. Button for each book, working on functions to extract book notes from book button clicked

---
+++ client/index.js
@@ -0,0 +1,3 @@
+function bookNotes() {
+    console.log("Function Worked")
+}
--- server/server.js
+++ server/server.js
@@ -3,13 +3,21 @@ const csvParser = require('csv-parser')
 const { error } = require('console')
 const millionDollarWeekend = "C:/Users/thoma/Downloads/milliondollarweekend.csv"; // Corrected path
 const dontBelieveEverthingYouThink = "c:/Users/thoma/Downloads/dontbelieveeverythingyouthink.csv"
+const howToBeFree = "c:/Users/thoma/Downloads/howtobefree.csv"
+const theWayToLove = "c:/Users/thoma/Downloads/thewaytolove.csv"
+
+
+// 1. we want to put each book into its own csv file
+
+// 2. We want to have a csv file with all our books highlighted notes. 
+
 
 const results = [];
-fs.createReadStream(dontBelieveEverthingYouThink)
+fs.createReadStream(theWayToLove)
   .pipe(csvParser())
   .on('data', (data) => results.push(data))
   .on('end', () => {
-    const jsonResults = JSON.stringify(results, null, 2);
+    const jsonResults = JSON.stringify(results, null, 2); 
     fs.writeFile('test.csv', jsonResults, (err) => {
       if (err) {
         console.log('Error writing to file:', err)
