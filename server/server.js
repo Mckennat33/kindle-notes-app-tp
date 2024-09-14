c21442dc54090ca920366028641fec2c04370e19
tom mckenna <thomasmckenna12@gmail.com>
2024-09-14 15:57:56 -0500

    looping through each book to send through parse notes, now I need to format the books then write their own unique file name

--- server/server.js
+++ server/server.js
@@ -10,20 +10,41 @@ const theWayToLove = "c:/Users/thoma/Downloads/thewaytolove.csv"
 // 2. need to put each book into a data structure so we can loop through them
 // I need to parse the notes before I can sent it through the format function 
 
-const book = [
+
+const books = [
   {path: 'C:/Users/thoma/Downloads/milliondollarweekend.csv', name:'millionDillorWeekend.csv'}, 
   {path: 'c:/Users/thoma/Downloads/dontbelieveeverythingyouthink.csv', name: 'dontBelieveEverythingYouThink.csv'}, 
   {path: 'c:/Users/thoma/Downloads/howtobefree.csv', name: 'howToBeFree.csv'}, 
   {path: 'c:/Users/thoma/Downloads/thewaytolove.csv', name: 'theWayToLove.csv'}
 ]
 
+books.forEach(book => formatBook(book))
 
-// book needs to be parsed before it can go through the formatBook function
 function formatBook(book) {
-  console.log(book)
+
+  // this is overwriting booknotes.csv file with the last book in the array. We need to creat a unique path/name for each book
+  // then we need to create a way to put them all in one. 
+  //first we need to format them 
+  const bookPath = book.path
+  console.log(bookPath)
+  const results = [];
+  fs.createReadStream(bookPath)
+  .pipe(csvParser())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    const jsonResults = JSON.stringify(results, null, 2); 
+    fs.writeFile('booknotes.csv', jsonResults, (err) => {
+      if (err) {
+        console.log('Error writing to file:', err)
+      } else {
+        console.log("file written successfully")
+      }
+    })
+  });
 }
 
-formatBook(book)
+
+// book needs to be parsed before it can go through the formatBook function
 
 function bookFile() {
 
@@ -33,21 +54,6 @@ function allBookFiles() {
 
 }
 
-const results = [];
-fs.createReadStream(theWayToLove)
-  .pipe(csvParser())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    const jsonResults = JSON.stringify(results, null, 2); 
-    console.log(jsonResults)
-    fs.writeFile('booknotes.csv', jsonResults, (err) => {
-      if (err) {
-        console.log('Error writing to file:', err)
-      } else {
-        console.log("file written successfully")
-      }
-    })
-  });
 
 
  
