a3a22ff02d35859030c8754d4fb52a3041c66663
Thomas <thomasmckenna12gmail.com>
2024-10-05 18:02:13 -0500

    Working on importing modules, having issues with path

--- server/server.js
+++ server/server.js
@@ -2,40 +2,29 @@ const fs = require('fs')
 const csvParser = require('csv-parser')
 const { error } = require('console');
 const { notStrictEqual } = require('assert');
-const millionDollarWeekend = "C:/Users/thoma/Downloads/milliondollarweekend.csv"; // Corrected path
-const dontBelieveEverthingYouThink = "c:/Users/thoma/Downloads/dontbelieveeverythingyouthink.csv"
-const howToBeFree = "c:/Users/thoma/Downloads/howtobefree.csv"
-const theWayToLove = "c:/Users/thoma/Downloads/thewaytolove.csv"
+const { create } = require('domain');
+const millionDollarWeekend = "/Users/thomasmckenna/Downloads/Million-Dollar-Weekend-Notes.csv"; // Corrected path
+const howToBeFree = "/Users/thomasmckenna/Downloads/how-to-be-free.csv"
 
 
 const books = [
-  {path: 'C:/Users/thoma/Downloads/milliondollarweekend.csv', name:'millionDillorWeekend.csv'}, 
-  {path: 'c:/Users/thoma/Downloads/dontbelieveeverythingyouthink.csv', name: 'dontBelieveEverythingYouThink.csv'}, 
-  {path: 'c:/Users/thoma/Downloads/howtobefree.csv', name: 'howToBeFree.csv'}, 
-  {path: 'c:/Users/thoma/Downloads/thewaytolove.csv', name: 'theWayToLove.csv'}
+  {path: "/Users/thomasmckenna/Downloads/Million-Dollar-Weekend-Notes.csv", name:'millionDillorWeekend.csv'}, 
+  {path: "/Users/thomasmckenna/Downloads/how-to-be-free.csv", name: 'howToBeFree.csv'}, 
 ]
-
 
 books.forEach(book => formatBook(book))
 
-// sending each book into formatBooks, so if I try to format it i need 
-//to do it for each book 
 function formatBook(book) {
-  // this is overwriting booknotes.csv file with the last book in the array. We need to creat a unique path/name for each book
-  // then we need to create a way to put them all in one. 
-  //first we need to format them 
   const bookPath = book.path
   const results = [];
   fs.createReadStream(bookPath)
   .pipe(csvParser())
   .on('data', (data) => results.push(data))
   .on('end', () => {
-    const newArray = results
-    const test = results.slice(7, results.length)
+    const bookNotes = results.slice(7, results.length)
     const [{"Your Kindle Notes For:": bookTitle}, {"Your Kindle Notes For:": author}] = results
-    //const [,,,,,,,{"": notes}] = newArray
 
-    const notes = test
+    const notes = bookNotes
     .slice(7)
     .filter(row => row["Your Kindle Notes For:"].startsWith("Highlight")) // Only get highlight rows
     .map(row => ({
@@ -43,31 +32,59 @@ function formatBook(book) {
       author: author,
       note: row[""] // Extract the actual note content
     }));
-    console.log(notes)
 
-  })
+
+    writeFile(notes)
+  })  
 }
 
+module.exports = { formatBook }
+
+function writeFile(booknotes) {
+    const stringNotes = JSON.stringify(booknotes)
+    fs.writeFile('test.csv', stringNotes, (err) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('success')
+      }
+    })
+    
+  }
 
 
-// book needs to be parsed before it can go through the formatBook function
-function createBookFile() {
-  fs.writeFile('booknotes.csv', jsonResults, (err) => {
-    if (err) {
-      console.log('Error writing to file:', err)
-    } else {
-      console.log("file written successfully")
-    }
-  })
-}
+
+  
+//   function readFiles(example) {
+//    fs.readFile('test.csv', 'utf-8', (err, data) => {
+//       const newData = JSON.parse(data)
+//       if (err) {
+//         console.log(err)
+//       } else {
+//         console.log("we did it", newData)
+//       }
+//    })
+//   }
+  
+//   readFiles()
 
 
-function bookFile(books) {
-
-}
-
-function allBookFiles() {
-
-}
 
 
+// const createCsvFile = require('csv-writer').createObjectCsvWriter
+// const csvWriter = createCsvFile({
+//   path: "allBookNotes.csv", 
+//   header: [
+//     {id: 'title', title: 'Title'},
+//     {id: 'author', title: 'Author'},
+//     {id: 'note', title: 'Note'},
+//   ]
+// })
+
+// csvWriter.writeRecords(notes)
+//   .then(() => {
+//     console.log(`File written for ${bookTitle}`)
+//   })
+//   .catch(err => {
+//     console.log("error writing CSV:", err)
+//   })
