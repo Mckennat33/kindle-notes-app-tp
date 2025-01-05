77664761ce9c5074f6f28c2db6380205b8c84f92
tom mckenna <thomasmckenna12@gmail.com>
2025-01-05 12:07:57 -0600

    added starter code for book and notes schemas

--- models/book.js
+++ models/book.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
+const { Schema } = mongoose
 
-
-const bookSchema = mongoose.Schema({
+const bookSchema = Schema({
     author: String, 
-    title: String
+    title: String, 
+    genre: String, 
+    bookId: Number
 })
 
 module.exports = mongoose.model('Book', bookSchema)
---
+++ models/highlightedNotes.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const highlightedNotes = Schema({
+    content: String, 
+    pageNumber: Number, 
+    book: String
+})
+
+module.exports = mongoose.model('Notes', highlightedNotes)
