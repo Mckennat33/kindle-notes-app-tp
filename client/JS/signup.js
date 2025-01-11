11ec5ad8150aba6231969cb5fc2e0d7eb57a986f
tom mckenna <thomasmckenna12@gmail.com>
2025-01-11 17:20:46 -0600

    added signup form code

--- client/JS/signup.js
+++ client/JS/signup.js
@@ -13,4 +13,35 @@ function signUp(event) {
     let userPassword = document.getElementById('password').value
     console.log(userName, userEmail, userPassword)
     
+
+    // i have the value for username, email and password. 
+    // need to storm them in a schema
+
+    const userAPI = "http://localhost:3000/signup"
+    async function postRequest() {
+        try {
+            const response = await fetch(userAPI, {
+                method: "POST",
+                headers: {
+                    "Content-Type": 'application/json'
+                }, 
+                body: JSON.stringify({
+                    username: userName, 
+                    email: userEmail, 
+                    password: userPassword
+                })
+            })
+            const data = await response.json()
+            if (response.ok) {
+                console.log(response.status)
+            } else {
+                console.log("Httpe Error", response.status)
+            }
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    postRequest()
+
 }
