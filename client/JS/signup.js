176bc06c66873943aa22b14f8ad69e3f5cf05cfb
tom mckenna <thomasmckenna12@gmail.com>
2025-01-11 14:13:20 -0600

    added some server side code

--- client/JS/signup.js
+++ client/JS/signup.js
@@ -1,33 +1,16 @@
 
-let signIn = document.querySelector('.sign-up-bttn')
-signIn.addEventListener('click', signUp)
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    let signUpButton = document.querySelector('sign-up-bttn')
-    signIn(addEventListener('click', signUp))
+    let signUpButton = document.querySelector('.sign-up-bttn')
+    signUpButton.addEventListener('click', signUp)
 })
 
-
-async function signUp(event) {
+function signUp(event) {
     event.preventDefault()
-    let userName = document.getElementById('username')
-    let userEmail = document.getElementById('email')
-    let userPassword = document.getElementById('password')
-
-    // button clicked
-    // fromvalidation
-    // if success move on to store the data
-        // store the data in a schema 
-        // send to the server side code 
-        // store the data in mongoose 
-    // if not success, error, send an arror meessage
-
-    try {
-        const response =  await fetch()
-
-
-    }
-
-
+    let userName = document.getElementById('username').value
+    let userEmail = document.getElementById('email').value
+    let userPassword = document.getElementById('password').value
+    console.log(userName, userEmail, userPassword)
+    
 }
-
