c4b7e176fb7bc62104b2a9aafa6ab3b47035b1a7
tom mckenna <thomasmckenna12@gmail.com>
2025-01-11 19:43:04 -0600

    added mongo db connection, however still need to work on the env file

--- server/mongo.js
+++ server/mongo.js
@@ -1,5 +1,9 @@
+
+
+const uri = "mongodb+srv://thomasmckenna12:Dwagon33%21%24%24@cluster0.guxft.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://thomasmckenna12:Dwagon33%21%24%24@cluster0.guxft.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -20,4 +24,4 @@ async function run() {
     await client.close();
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
