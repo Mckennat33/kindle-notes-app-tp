d842eb02099fae3ffd7b93f441c1c0dc9079006d
tom mckenna <thomasmckenna12@gmail.com>
2025-01-21 20:06:44 -0600

    added logic to save user and throw an error if username already exists.

--- controllers/signup.controller.js
+++ controllers/signup.controller.js
@@ -6,9 +6,12 @@ const mongoose = require('mongoose')
 require('dotenv').config({ path: '../.env ' });
 app.use(express.json())
 const User = require("../models/user.js")
-const homeRoute = require('../routes/home.routes.js')
+const loginRoute = require('../routes/login.routes.js')
 const bcrypt = require('bcrypt')
 const { error } = require('console')
+
+app.use('/login', loginRoute)
+
 
 const signupPage = async (req, res) => {
     try {
@@ -21,28 +24,48 @@ const signupPage = async (req, res) => {
 
 
 const userSignup = async (req, res) => {
-  const {username, email, password } = req.body
-  const hashedPassword = bcrypt.hash(password, 10)
-  
+  // take in username email password
+  const { username, email, password} = req.body
+  // console.log(username, email, password)
   try {
-    const newUser = new User({
-      username, 
-      email,    
-      hashedPassword
-    })
-
-    console.log(hashedPassword)
-    await newUser.save()
-    
-
-  } catch(err) {
-    console.log(JSON.stringify(err))
-    if (err.code === 11000) {
-      return res.json({ status: 'error', err: 'duplicate username'})
+    const matchingUsername = await User.findOne({ username })
+    if (matchingUsername) {
+      console.log('Username already exists')
+      return res.status(400).json({ message: 'username already exists'}) 
+    } else {
+      const saltRounds = 10; 
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      const newUser = new User({
+        username, 
+        email, 
+        password: hashedPassword
+      })
+      await newUser.save()
     }
-    console.log('new user created')
+  } catch(err) {
+    return res.json({message: err.message})
   }
 }
+
+// const userSignup = async (req, res) => {
+//   const {username, email, password } = req.body
+//   const hashedPassword = bcrypt.hash(password, 10)
+//   try {
+//     const newUser = new User({
+//       username, 
+//       email,    
+//       hashedPassword
+//     })
+//     console.log(hashedPassword)
+//     await newUser.save()
+//   } catch(err) {
+//     console.log(JSON.stringify(err))
+//     if (err.code === 11000) {
+//       return res.json({ status: 'error', err: 'duplicate username'})
+//     }
+//     console.log('new user created')
+//   }
+// }
 
 module.exports = {
     signupPage,
