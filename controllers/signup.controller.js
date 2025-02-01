2deb3769e56e0832e249b1a83d0f66cf4c3adffb
tom mckenna <thomasmckenna12@gmail.com>
2025-01-31 20:25:26 -0600

    fixed bug: JWT token and duplicate users

--- controllers/signup.controller.js
+++ controllers/signup.controller.js
@@ -1,22 +1,24 @@
+
 const jwt = require('jsonwebtoken')
 const express = require('express')
 const app = express()
 const router = express.Router()
 const _ = require('lodash')
 const path = require('path')
-const mongoose = require('mongoose')0
-require('dotenv').config({ path: '../.env ' });
-app.use(express.json())
+const mongoose = require('mongoose')
+require('dotenv').config({ path: '../.env' });
 const User = require("../models/user.js")
 const loginRoute = require('../routes/login.routes.js')
 const bcrypt = require('bcrypt')
 const { error } = require('console')
 app.use('/login', loginRoute)
-
+const config = require('config')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 const signupPage = async (req, res) => {
     try {
-        res.sendFile(path.join(__dirname, '../client/html/signup.html'))
+        await res.sendFile(path.join(__dirname, '../client/html/signup.html'))
       } catch(err) {
         console.log(err)
         res.status(500).send("internal Server Error")
@@ -29,8 +31,7 @@ const userSignup = async (req, res) => {
     const matchingUsername = await User.findOne({ username })
     if (matchingUsername) {
       console.log('Username already exists')
-      
-     return res.status(400).json({ message: 'username already exists'}) 
+      return res.status(400).json({ message: 'username already exists'}) 
     } else {
       const saltRounds = 10; 
       const hashedPassword = await bcrypt.hash(password, saltRounds);
@@ -40,12 +41,19 @@ const userSignup = async (req, res) => {
         password: hashedPassword
       })
       await newUser.save()
-      res.send({
-        username, email
-      })
+      console.log(newUser)
+      // create a jwt 
+      const jwtPrivateKey = process.env.JWT_PRIVATE_KEY;
+      if (!jwtPrivateKey) {
+        console.error("FATAL ERROR: jwtPrivateKey is not defined");
+        process.exit(1);
+      }
+      const token = jwt.sign({ userId: newUser._id }, jwtPrivateKey, { expiresIn: '1hr'})
+      console.log("Signup successful, sending response...");
+      res.json({ message: 'User created successfully!', username: newUser.username, email: newUser.email, token });
     }
   } catch(err) {
-    return res.json({message: err.message})
+    return res.status(500).json({message: err.message})
   }
 }
 
