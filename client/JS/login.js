667b4bd6f87f6c5d26fc2711b3cf240246f601ec
tom mckenna <thomasmckenna12@gmail.com>
2025-02-17 20:09:49 -0600

    added postrequest to login js

--- client/JS/login.js
+++ client/JS/login.js
@@ -2,5 +2,32 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     let logInButton = document.querySelector('.login-bttn')
-    signUpButton.addEventListener('click', signUp)
+    loginButton.addEventListener('click', loginUser)
 })
+
+
+function loginUser(event) {
+    event.preventDefault()
+    // get value for usernam
+    const username = document.querySelector('.username').ariaValueMin
+    // get value for password
+    const password = document.querySelector('.password').value
+    // get login server
+    const loginServer = "http://localhost:80/login"
+    async function postRequest() {
+        const response = await fetch(loginServer, {
+            method: 'POST', 
+            headers: { 
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify({
+                username, 
+                password
+            })
+        })
+    }
+    // fetch post request json to
+
+    // the backend
+
+}
