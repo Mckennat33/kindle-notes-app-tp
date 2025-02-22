95790e742997684b03f0c8bcba7f170b5906420c
tom mckenna <thomasmckenna12@gmail.com>
2025-02-22 17:29:17 -0600

    added ignore initial add or events

--- services/catchnote.js
+++ services/catchnote.js
@@ -1,9 +1,20 @@
 const chokidar = require('chokidar')
 
+// when something is downloaded or sent to the download file
+// if it is a csv file 
+    // read that data
+    // parse data and take the informaiton that we need
+    // put that info in our database
+// if not csv file 
+    // ignore
+
 const testFunction = () => {
     try {    
         const watcher = chokidar.watch('C:/Users/thoma/Downloads', 
-            {persistent: true}
+            {
+                persistent: true, 
+                ignoreInitial: true
+            }
         )
         watcher.on('add', (path) => {
             console.log(path, "File downloaded")
@@ -21,19 +32,4 @@ testFunction()
 module.exports = {
     testFunction
 }
-
-
-
-
-
-
-
-
-// when something is downloaded or sent to the download file
-// if it is a csv file 
-    // read that data
-    // parse data and take the informaiton that we need
-    // put that info in our database
-// if not csv file 
-    // ignore
 
