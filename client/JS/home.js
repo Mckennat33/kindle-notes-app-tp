954ae74d44230432bfc0d0a33624fa9bc1c7adc9
tom mckenna <thomasmckenna12@gmail.com>
2025-03-15 22:03:54 -0500

    BUGFIX: added second function to fetch user data

--- client/JS/home.js
+++ client/JS/home.js
@@ -1,27 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-    window.addEventListener('load', () => {
-        // what am i fetching? 
-            // 
-        // Make a fetch request for users information 
-        const homeFetchData = "http://localhost:80/home"
-        async function fetchUserData() {
-            const promis = await fetch(homeFetchData, {
-                method: GET, 
-                headers: {
-                    // token is stored in the authentication header"
-                        // how do i get this token from the backend
-                    "Authorization": `Bearer ${token}`,
-                    "Content-Type": 'application/json'
-                },
-                body: JSON.stringify({ })
-            })
-        }
+        const homeFetch = "http://localhost:80/home"
+        const fetchUserData = "http://localhost:80/home/data"
         
-            // token authorization in the header of the fetch request. 
-        // sent token to backend to verify that we have access to this information
+        async function fetchHome() {
+            try {
+                const response = await fetch(homeFetch, {
+                method: 'GET'
+                // credentials: 'include',
+                // headers: {
+                //     "Content-Type": 'application/json'
+                // }
+            })
 
-        console.log("Home page Loaded")
-    })
+            const resp = await response.text()
+            if (response.ok) {
+                console.log("Home Page Loaded")
+            } else {
+                console.log("there was an error", response.status, 'This did not work')
+            }
+        } catch(err) {
+            console.log("Fetch Error", err.message)
+        }
+    }
+        fetchHome()
+
+        async function fetchData() {
+            try {   
+                const response = await fetch(fetchUserData, {
+                    method: 'GET', 
+                    credentials: 'include', 
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+
+                })
+                const jsonData = await response.json()
+                if (response.ok) {
+                    console.log(jsonData, "Retreived data from mongodb")
+                } else {
+                    console.log("Error from fetching User Data", response.status)
+                }
+            } catch(err) {
+                console.log("there was an error", err.message)
+            }
+        }
+        fetchData()
 })
 
  
