d29ce651b9b8cff66e14297debb20ad7b311f158
tom mckenna <thomasmckenna12@gmail.com>
2025-03-21 21:29:34 -0500

    debug: 1. errors matching title with downloaded book 2. Need to update format for destructured book

--- services/catchnote.js
+++ services/catchnote.js
@@ -56,6 +56,7 @@ const parseBook = async (path) => {
             const matchingBook = await Book.findOne({title: bookTitle})
             if (matchingBook) {
                 console.log("Book already exists")
+
             } else {
                 const newBook = new Book({
                     author: author, 
@@ -73,23 +74,39 @@ const parseBook = async (path) => {
 
 
 const parsePdfBook = async (path) => {
-    let dataBuffer = fs.readFileSync(path)
-    pdf(dataBuffer).then(function(data) {
-        const { text } = data
-        const pdfNotes = text.split("Free")[1].split('|').slice(2)
+    try {
 
-        const pdfTitle = text.split('Free')[0].split('by')[0]
-
-        const pdfAuthor = text.split('Free')[0].split('by')[1]
-
-        const bookObject = {
-        author: pdfAuthor, 
-        title: pdfTitle,
-        notes: pdfNotes
+        let dataBuffer = fs.readFileSync(path)
+        pdf(dataBuffer).then(function(data) {
+            
+            const { text } = data
+            const pdfNotes = text.split("Free")[1].split('|').slice(2)
+            const pdfTitle = text.split('Free')[0].split('by')[0]
+            const pdfAuthor = text.split('Free')[0].split('by')[1]
+            
+            const matchingPdfBook = Book.find({text: pdfTitle})
+            if (matchingPdfBook) {
+                console.log('PDF Book already exists')
+            } else {
+                const pdfBook = new Book({
+                    author: pdfAuthor, 
+                    title: pdfTitle, 
+                    note: pdfNotes
+                })
+                pdfBook.save()
+            }
+            
+            // const bookObject = {
+                // author: pdfAuthor, 
+                // title: pdfTitle,
+                // notes: pdfNotes
+                // }
+                
+                console.log()
+            })
+        } catch (err) {
+            console.log(err.message)
         }
-
-        console.log(bookObject)
-    })
 }
 
 catchDownloadedNote()
