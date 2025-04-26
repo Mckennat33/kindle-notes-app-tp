b870631cae4df7be10c781678265c5b9ff67b478
tom mckenna <thomasmckenna12@gmail.com>
2025-04-25 19:33:38 -0500

    init code for logging submited book form

--- controllers/addBookForm.controller.js
+++ controllers/addBookForm.controller.js
@@ -6,6 +6,8 @@ const addBook = async (req, res) => {
     console.log(req.body)
     console.log(title, author, notes)
     console.log('this worked from the front end')
+
+    res.status(200).json({ message: 'Book added successfully' })
 }
 
 module.exports = {
