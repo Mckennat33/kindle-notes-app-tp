878c9fa2aef7717f9450ec88dac45f50131d9798
tom mckenna <thomasmckenna12@gmail.com>
2025-04-26 16:17:54 -0500

    added server for sumbitting a manual book

--- client/JS/home.js
+++ client/JS/home.js
@@ -194,7 +194,7 @@ notes.forEach((note, index) => {
 
     // Book Form Function
     const submitBook = document.querySelector('.pop-form')
-    const loginServer = "http://localhost:80/home"
+    const loginServer = "http://localhost:80/home/submit"
     submitBook.addEventListener('submit', (event) => {
         event.preventDefault()
         const manAuthor = document.getElementById('author').value
