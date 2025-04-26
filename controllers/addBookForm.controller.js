db11f3852d0d736c7dc2b117bf3c1826e40856f2
tom mckenna <thomasmckenna12@gmail.com>
2025-04-26 16:17:04 -0500

    init code for saving a manually added book. Bug: saving the book to the database

--- controllers/addBookForm.controller.js
+++ controllers/addBookForm.controller.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const path = require('path')
+const Book = require('../models/book.js')
 
 const addBook = async (req, res) => {
     const {title, author, notes} = req.body
-    console.log(req.body)
     console.log(title, author, notes)
     console.log('this worked from the front end')
+    try {
+
+        const manBook = new Book({
+            title: title, 
+            author: author, 
+            notes: notes
+        })
+        
+        await manBook.save()
+    } catch(err) {
+        console.log(err, "there was an error saving your manual book")
+    }
 
     res.status(200).json({ message: 'Book added successfully' })
 }
