1338529844a40f27a915bd7cf4af84cbe0a6f3c7
tom mckenna <thomasmckenna12@gmail.com>
2025-04-26 16:36:52 -0500

    bug fix: saved book to database and updated error handling

--- controllers/addBookForm.controller.js
+++ controllers/addBookForm.controller.js
@@ -4,17 +4,21 @@ const Book = require('../models/book.js')
 
 const addBook = async (req, res) => {
     const {title, author, notes} = req.body
-    console.log(title, author, notes)
-    console.log('this worked from the front end')
+    // console.log(title, author, notes)
+    // console.log('this worked from the front end')
     try {
-
-        const manBook = new Book({
-            title: title, 
-            author: author, 
-            notes: notes
-        })
-        
-        await manBook.save()
+        const matchingBook = await Book.findOne({ title: title })
+        if (matchingBook) {
+            console.log('book already exists')
+        }  else {
+            const manBook = new Book({
+                title: title, 
+                author: author, 
+                notes: notes
+            })    
+            console.log(manBook)
+            await manBook.save()
+        }
     } catch(err) {
         console.log(err, "there was an error saving your manual book")
     }
