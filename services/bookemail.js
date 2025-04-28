0399e0237dfde10e2c0daa8c52821bf65c3faf83
tom mckenna <thomasmckenna12@gmail.com>
2025-04-28 18:59:58 -0500

    init code for getting all book data. Bug: issue pulling and connecting to the database

--- services/bookemail.js
+++ services/bookemail.js
@@ -1,5 +1,23 @@
 // look at docs on how to use
 const schedule = require('node-schedule');
+
 // look at docs on how to use
 const nodemailer = require('nodemailer')
+const express = require('express')
+const connectDB = require("../config/dbconn.js")
+const mongoose = require('mongoose')
+require('dotenv').config({ path: '../.env' });
+const Books = require('../models/book.js')
 
+
+async function getAllBooks() {
+    try {
+      const allBooks = await Books.find();
+      console.log(allBooks);
+    } catch (err) {
+      console.error('Error fetching books:', err);
+    }
+  }
+  
+  getAllBooks();
+  
