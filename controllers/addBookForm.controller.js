7a7d64946f4c8eba6fe5c18f24790785de193a70
tom mckenna <thomasmckenna12@gmail.com>
2025-04-29 19:41:05 -0500

    adding manual book and notes to a book

--- controllers/addBookForm.controller.js
+++ controllers/addBookForm.controller.js
@@ -1,35 +1,36 @@
-const express = require('express')
-const path = require('path')
-const Book = require('../models/book.js')
+const express = require('express');
+const path = require('path');
+const Book = require('../models/book.js');
 
 const addBook = async (req, res) => {
-    const {title, author, notes} = req.body
-    // console.log(title, author, notes)
-    // console.log('this worked from the front end')
+    const { title, author, notes } = req.body;
+
     try {
-        const matchingBook = await Book.findOne({ title: title, author: author })
+        const matchingBook = await Book.findOne({ title: title, author: author });
+
         if (matchingBook) {
-            console.log('book already exists')
-            Book.updateOne(
-                {author: author},
-                { $push: { notes: notes }}
-            )
+            await Book.findOneAndUpdate(
+                { title: title, author: author },
+                { $push: { notes: notes } }
+            );
         } else {
             const manBook = new Book({
-                title: title, 
-                author: author, 
+                title: title,
+                author: author,
                 notes: notes
-            })    
-            console.log(manBook)
-            await manBook.save()
+            });
+            console.log(manBook);
+            await manBook.save();
         }
-    } catch(err) {
-        console.log(err, "there was an error saving your manual book")
-    }
 
-    res.status(200).json({ message: 'Book added successfully' })
-}
+        res.status(200).json({ message: 'Book added successfully' });
+    } catch (err) {
+        console.log(err, "there was an error saving your manual book");
+        res.status(500).json({ error: 'Failed to add book' });
+    }
+};
 
 module.exports = {
     addBook
-}
+};
+
