4a5206653625d01248a78432e2f012c9192fb832
tom mckenna <thomasmckenna12@gmail.com>
2025-05-19 19:08:58 -0500

    added notes

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -10,6 +10,9 @@ const addBookForm = require('../controllers/addBookForm.controller.js')
 router.get("", homeController.homePage)
 router.get("/data", homeController.getUserData)
 router.post("/submit", addBookForm.addBook)
+
+
+// not sure if these below were notes out for a reason 
 // router.get('/', homeController.getUserData);
 
 // router.get("", homeController.catchNote)
