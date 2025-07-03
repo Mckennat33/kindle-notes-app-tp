4b33c364f1ed16ed305f50fdc712cca0aa98d95e
tom mckenna <thomasmckenna12@gmail.com>
2025-07-02 19:06:57 -0500

    init grabbed access_token from cookies to verify user

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -3,7 +3,10 @@ const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
 const catchNote = require('../services/catchnote.js')
+const jwt = require('jsonwebtoken')
 const Book = require("../models/book.js")
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
 const homePage = async (req, res) => { 
     try {
@@ -16,7 +19,16 @@ const homePage = async (req, res) => {
 
 const getUserData = async (req, res) => {
     try {
+    
+        const token = req.cookies.access_token
+        if(!token) return res.status(401).json({message: 'no token provided'})
+
+        const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+        console.log(decoded)
+
         const bookInfo = await Book.find({  })
+
+        console.log(bookInfo.userId)
 
         res.json(bookInfo)
     } catch(err) {
