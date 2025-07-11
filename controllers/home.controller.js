23703f6d504db48ddc0c2c957b52500c19c31c67
tom mckenna <thomasmckenna12@gmail.com>
2025-07-10 19:12:55 -0500

    no longer finding book data by id

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -19,16 +19,10 @@ const homePage = async (req, res) => {
 
 const getUserData = async (req, res) => {
     try {
-    
         const token = req.cookies.access_token
         if(!token) return res.status(401).json({message: 'no token provided'})
-
         const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
-
-        const bookInfo = await Book.find({ userId: decoded.userId })
-        console.log(bookInfo)
-        // console.log(bookInfo.userId)
-
+        const bookInfo = await Book.find({ })
         res.json(bookInfo)
     } catch(err) {
         console.log(err)
